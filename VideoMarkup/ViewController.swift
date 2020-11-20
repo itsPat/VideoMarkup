5ae39785f49c5de4f39cf4499b63e38b8a017549
Pat Trudel <patrick.trudel@me.com>
2020-11-20 11:32:07 -0500

    Initial Commit

---
+++ VideoMarkup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoMarkup
+//
+//  Created by Pat Trudel on 2020-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
